7b8a2ebbb73421aca819686ffb80a4f740be6a29
Sebastian Sigvard <sebastian.sigvard@ptech.com.ar>
2022-09-17 20:09:24 -0300

    properly set of evenet listeners, added manifiesto and logo images

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -8,10 +8,15 @@ export default class Navbar extends React.Component {
         wWidth: window.innerWidth,
         menuDisplayed: false,
     };
+    this.resizeHandler = () => {this.setState({wWidth: window.innerWidth});}
+  }
 
-    window.addEventListener('resize', () => {
-        this.setState({wWidth: window.innerWidth});
-    });
+  componentDidMount(){
+    window.addEventListener('resize', this.resizeHandler);
+  }
+  
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.resizeHandler);
   }
 
   render() {
@@ -19,19 +24,19 @@ export default class Navbar extends React.Component {
     displayNav = this.state.menuDisplayed ? "flex" : displayNav;
 
     return (
-        <div class="navbar">
-            <div class="container">
-                <h1 class="navbar-name"><span>Sig</span>vard</h1>
-                <img id="mobile-cta" class="mobile-menu" src="images/menu.svg" alt="Open navegation menu" onClick={()=>this.setState({menuDisplayed: true})}/>
+        <div className="navbar">
+            <div className="container">
+                <h1 className="navbar-name"><span>Sig</span>vard</h1>
+                <img id="mobile-cta" className="mobile-menu" src="images/menu.svg" alt="Open navegation menu" onClick={()=>this.setState({menuDisplayed: true})}/>
                 <nav style={{display : displayNav}}>
-                    <img id="mobile-exit" class="mobile-menu-exit" src="images/x.svg" alt="Close navegation menu" onClick={()=>this.setState({menuDisplayed: false})}/>
-                    <ul class="primary-nav">
+                    <img id="mobile-exit" className="mobile-menu-exit" src="images/x.svg" alt="Close navegation menu" onClick={()=>this.setState({menuDisplayed: false})}/>
+                    <ul className="primary-nav">
                         <li><a href="home">Home</a></li>
                         <li><a href="calcount">Calories Counter</a></li>
                         <li><a href="market-status">Market Status</a></li>
                     </ul>
-                    <ul class="secondary-nav">
-                        <li class="contact-cta"><a href="contact">Contact me</a></li>
+                    <ul className="secondary-nav">
+                        <li className="contact-cta"><a href="contact">Contact me</a></li>
                     </ul>
                 </nav>
             </div>
