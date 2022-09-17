0b2033cbeb2800b3700405133c9a5e10592d66cc
Sebastian Sigvard <sebastian.sigvard@ptech.com.ar>
2022-09-17 12:43:58 -0300

    adding dynamic style and rendering to navbar

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -3,22 +3,34 @@ import React from 'react'
 export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {favoritecolor: "red"};
+    this.state = {
+        wWidth: window.innerWidth,
+        menuDisplayed: false,
+    };
+
+    window.addEventListener('resize', () => {
+        this.setState({wWidth: window.innerWidth});
+    });
   }
 
   render() {
+    let displayNav = this.state.wWidth > 870 ? "flex" : "none";
+    displayNav = this.state.menuDisplayed ? "flex" : displayNav;
+
     return (
         <div class="navbar">
             <div class="container">
                 <h1 class="navbar-name"><span>Sig</span>vard</h1>
-                <nav>
+                <img id="mobile-cta" class="mobile-menu" src="images/menu.svg" alt="Open navegation menu" onClick={()=>this.setState({menuDisplayed: true})}/>
+                <nav style={{display : displayNav}}>
+                    <img id="mobile-exit" class="mobile-menu-exit" src="images/x.svg" alt="Close navegation menu" onClick={()=>this.setState({menuDisplayed: false})}/>
                     <ul class="primary-nav">
-                        <li class="current">Home</li>
-                        <li><a href="calcount.html">Calories Counter</a></li>
-                        <li><a href="market-status.html">Market Status</a></li>
+                        <li><a href="home">Home</a></li>
+                        <li><a href="calcount">Calories Counter</a></li>
+                        <li><a href="market-status">Market Status</a></li>
                     </ul>
                     <ul class="secondary-nav">
-                        <li class="contact-cta"><a href="contact.html">Contact me</a></li>
+                        <li class="contact-cta"><a href="contact">Contact me</a></li>
                     </ul>
                 </nav>
             </div>
