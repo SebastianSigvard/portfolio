4af4bc9900745c56fa0e1fb5cc9e98ecd9067fd1
SebastianSigvard <sebastiansigvard@gmail.com>
2022-09-18 11:47:07 -0300

    added home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import './home.css'
 
 export default class Home extends React.Component {
     render(){
-        return ( <h1>Home</h1>);
+        return (
+            <section class="hero">
+            <div class="container">
+                <div class="main-panel">
+                    <h1 class="hero-name"><a href="/">Sebastián Sigvard</a></h1>
+                    <p class="subhead">Blockchain Developer</p>
+                    <a href="https://www.linkedin.com/in/claudio-sebastian-sigvard/"><img src="images/linkedin.svg" alt="linkedin"/></a>
+                    <a href="https://github.com/SebastianSigvard"><img src="images/github.svg" alt="github"/></a>
+                    <a href="https://www.instagram.com/sebasig/"><img src="images/instagram.svg" alt="github"/></a>
+                </div>
+            </div>
+            </section>
+        );
     }
 }
