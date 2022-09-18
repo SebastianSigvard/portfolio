035b5ddbd1a1c372202d8cee3df4d7e3e410d202
SebastianSigvard <sebastiansigvard@gmail.com>
2022-09-18 20:52:47 -0300

    jsx and css style for contact page

--- src/contact-me/ContactMe.js
+++ src/contact-me/ContactMe.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import './contact-me.css'
 
 export default class ContactMe extends React.Component {
     render(){
-        return ( <h1>Contact Me</h1>);
+        return (
+        <section class="hero">
+            <div class="container">
+                <h2>Send Me a Message</h2>
+                <form id="contact-form" class="main-panel">
+                    <label><strong>Name</strong></label> 
+                    <br/>  
+                    <input id="contact-name" type="text" name="name" required />
+                    <br/>
+                    <label><strong>Your Email</strong></label>
+                    <br/>
+                    <input id="contact-email" type="text" name="email" required />
+                    <br/>
+                    <label><strong>Message</strong></label>
+                    <br/>
+                    <textarea id="contact-message" name="message"></textarea>
+                    <br/>
+                    <button id="send-button" type="submit">Send</button>
+                </form>        
+            </div>
+        </section>
+        );
     }
 }
