7cfe72879b6cd09ec39140e43ca9c629cca35e7a
SebastianSigvard <sebastiansigvard@gmail.com>
2022-09-24 08:41:32 -0300

    CalCounter encapsulation some elements

--- src/cal-counter/CalCounter.js
+++ src/cal-counter/CalCounter.js
@@ -1,66 +1,52 @@
 import React from 'react'
 import './calCounter.css'
 
-export default class CalCounter extends React.Component {
-    render(){
-        return ( 
-            <section className="hero-calcount">
-            <div className="container">
-                <h2 id="cal-title">Calories Counter</h2>
-    
-                <div className="container" id="main">
-                    <h2>Add food</h2>
-                    <form id="create-form">
-                      <div className="nutrition-values">
-                        <label for="create-name">Food name:</label>
-                        <input id="create-name" type="text/" placeholder="Name"/>
-                        <label for="create-carbs">Carbs:</label>
-                        <input
-                          className="nutrition-input"
-                          type="number"
-                          id="create-carbs"
-                          min="0"
-                          max="200"
-                          placeholder="0g"
-                        />
-                        <label for="create-protein">Protein:</label>
-                        <input
-                          className="nutrition-input"
-                          type="number"
-                          id="create-protein"
-                          min="0"
-                          max="200"
-                          placeholder="0g"
-                        />
-                        <label for="create-fat">Fat:</label>
-                        <input
-                          className="nutrition-input"
-                          type="number"
-                          id="create-fat"
-                          min="0"
-                          max="200"
-                          placeholder="0g"
-                        />
-                        <div></div>
-                      </div>
-                      <input
-                        type="submit"
-                        className="btn btn-default create-submit"
-                        value="Add"
-                      />
-                    </form>
-                    <h2>Stats</h2>
-                    <canvas id="app-chart" width="100%"></canvas>
-                    <h2>Log</h2>
-                    <div className="card" id="total-calories-container">
-                      <h3>Total calories logged: <span id="total-calories">0</span></h3>
-                    </div>
-                    <ul id="food-list"></ul>
-                </div>
-    
-                <button id="clean-entrys">Clean Entrys</button><button id="log-out">LogOut</button>
-            </div>
-        </section>
-        );
+class CalCountApp extends React.Component {
+  render(){
+    const num_input_properties = {
+      className:"nutrition-input",
+      type: "number",
+      min:"0",
+      max:"200",
+      placeholder:"0g"
     }
+
+    return (
+      <div className="container">
+        <h2 id="cal-title">Calories Counter</h2>
+        <div className="container" id="main">
+            <h2>Add food</h2>
+            <form id="create-form">
+              <div className="nutrition-values">
+                <label for="create-name">Food name:</label>  <input id="create-name" type="text/" placeholder="Name"/>
+                <label for="create-carbs">Carbs:</label>     <input id="create-carbs" {... num_input_properties} />
+                <label for="create-protein">Protein:</label> <input id="create-protein" {... num_input_properties} />
+                <label for="create-fat">Fat:</label>         <input id="create-fat" {... num_input_properties} />
+              </div>
+              <input type="button" className="btn btn-default create-submit" value="Add"/>
+            </form>
+
+            <h2>Stats</h2>
+            <canvas id="app-chart" width="100%"></canvas>
+
+            <h2>Log</h2>
+            <div className="card" id="total-calories-container">
+              <h3>Total calories logged: <span id="total-calories">0</span></h3>
+            </div>
+            <ul id="food-list"></ul>
+        </div>
+        <button id="clean-entrys">Clean Entrys</button><button id="log-out">LogOut</button>
+      </div>
+    );
+  }
+}
+
+export default class CalCounter extends React.Component {
+  render(){
+    return ( 
+      <section className="hero-calcount">
+        <CalCountApp />
+      </section>
+    );
+  }
 }
