7a77424d89d2408fb2c9d07712088b47e202ef7c
SebastianSigvard <sebastiansigvard@gmail.com>
2022-09-24 09:34:35 -0300

    navigation through app login and registration

--- src/cal-counter/CalCounter.js
+++ src/cal-counter/CalCounter.js
@@ -43,28 +43,36 @@ class CalCountApp extends React.Component {
 }
 
 class CalCountLogin extends React.Component {
-  constructor(props){
-    super(props);        
-
-    this.state = {
-        token : '',
-    }
-  }
-
   render(){
     return(
-    <div>
-      <h1>CalCountLogin</h1>
-      <button onClick={this.props.handleLogin}>Login</button>
-    </div>);
+      <div className="container">
+        <h2>LOGIN</h2>
+        <form id="login-form" className="main-panel">
+            <label><strong>Username</strong></label> 
+            <br/>  
+            <input id="user-name" type="text" placeholder="Enter Username" name="username" required/>
+            <br/>
+            <label><strong>Password</strong></label>
+            <br/>
+            <input id="password" type="password" placeholder="Enter Password" name="password" required/>
+            <br/>
+            <button id="submit-button" type="submit" onClick={this.props.handleLogin}>Login</button>
+            <button id="go-registration-button" onClick={this.props.handleGoToRegister}>Register</button>
+        </form>
+      </div>
+    );
   }
 }
 
-// class CalCountRegister extends React.Component {
-//   render(){
-//     return(<h1>CalCountRegister</h1>);
-//   }
-// }
+class CalCountRegister extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>CalCountRegister</h1>
+        <button id="go-login-button" onClick={this.props.handleGoToLogin}>Login</button>
+      </div>);
+  }
+}
 
 export default class CalCounter extends React.Component {
   constructor(){
@@ -72,9 +80,12 @@ export default class CalCounter extends React.Component {
 
     this.state = {
         token : localStorage.getItem('token'),
+        onLogin: true
     }
     this.handleLogin = this.handleLogin.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleGoToRegister = this.handleGoToRegister.bind(this);
+    this.handleGoToLogin = this.handleGoToLogin.bind(this);
   }
 
   handleLogin(){
@@ -87,11 +98,20 @@ export default class CalCounter extends React.Component {
     this.setState({token: ''});
   }
 
+  handleGoToRegister(){
+    this.setState({onLogin: false});
+  }
+
+  handleGoToLogin(){
+    this.setState({onLogin: true});
+  }
+
   render(){
     return ( 
       <section className="hero-calcount">
         {this.state.token && <CalCountApp handleLogout={this.handleLogout}/>}
-        {!this.state.token && <CalCountLogin handleLogin={this.handleLogin}/>}
+        {!this.state.token && this.state.onLogin && <CalCountLogin handleLogin={this.handleLogin} handleGoToRegister={this.handleGoToRegister}/>}
+        {!this.state.token && !this.state.onLogin && <CalCountRegister handleGoToLogin={this.handleGoToLogin}/>}
       </section>
     );
   }
