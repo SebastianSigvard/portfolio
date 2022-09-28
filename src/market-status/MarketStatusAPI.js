787ad285b4f5c76684d759fe372bde06250ca473
SebastianSigvard <sebastiansigvard@gmail.com>
2022-09-28 11:35:36 -0300

    market status cards style

--- src/market-status/MarketStatusAPI.js
+++ src/market-status/MarketStatusAPI.js
@@ -6,27 +6,27 @@ class Card extends React.Component {
   render(){
     if(this.props.status === 'success'){
       return(
-        <li class="card">
+        <li className="card-market-status">
           <div>
-            <h3 class="name">{this.props.currencyPair}</h3>
+            <h3 className="name">{this.props.currencyPair}</h3>
             <h4>Bid</h4>
-            <ul class="macros">
-              <li class="carbs"><div>Quantity</div><div class="value">{this.props.data.bid.quantity}</div></li>
-              <li class="protein"><div>Rate</div><div class="value">{this.props.data.bid.rate}</div></li>
+            <ul className="macros">
+              <li className="carbs"><div>Quantity</div><div className="value">{this.props.data.bid.quantity}</div></li>
+              <li className="protein"><div>Rate</div><div className="value">{Number(this.props.data.bid.rate).toFixed(2)}</div></li>
             </ul>
             <h4>Ask</h4>
-            <ul class="macros">
-              <li class="carbs"><div>Quantity</div><div class="value">{this.props.data.ask.quantity}</div></li>
-              <li class="protein"><div>Rate</div><div class="value">{this.props.data.ask.rate}</div></li>
+            <ul className="macros">
+              <li className="carbs"><div>Quantity</div><div className="value">{this.props.data.ask.quantity}</div></li>
+              <li className="protein"><div>Rate</div><div className="value">{Number(this.props.data.ask.rate).toFixed(2)}</div></li>
             </ul>
           </div>
         </li>
       );
     } else {
       return(
-        <li class="card">
+        <li className="card">
           <div>
-            <h3 class="name">Error</h3>
+            <h3 className="name">Error</h3>
             <h4>{this.props.message}</h4>
           </div>
         </li>
