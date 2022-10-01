65dc48a61102afbb9e9d97f17d661cd41553f308
SebastianSigvard <sebastiansigvard@gmail.com>
2022-10-01 10:09:54 -0300

    encapsulation of market status cards

--- src/market-status/MarketStatusWEBS.js
+++ src/market-status/MarketStatusWEBS.js
@@ -1,37 +1,5 @@
 import React from 'react'
-
-class CardTips extends React.Component {
-  render(){
-    if(this.props.status === 'success'){
-      return(
-        <li className="card-market-status">
-          <div>
-            <h3 className="name">{this.props.currencyPair}</h3>
-            <h4>Bid</h4>
-            <ul>
-              <li><div><strong>Quantity</strong></div><div>{this.props.data.bid.quantity}</div></li>
-              <li><div><strong>Rate</strong></div><div>{Number(this.props.data.bid.rate).toFixed(2)}</div></li>
-            </ul>
-            <h4>Ask</h4>
-            <ul>
-              <li><div><strong>Quantity</strong></div><div>{this.props.data.ask.quantity}</div></li>
-              <li><div><strong>Rate</strong></div><div>{Number(this.props.data.ask.rate).toFixed(2)}</div></li>
-            </ul>
-          </div>
-        </li>
-      );
-    } else {
-      return(
-        <li className="card">
-          <div>
-            <h3 className="name">Error</h3>
-            <h4>{this.props.message}</h4>
-          </div>
-        </li>
-      );
-    }
-  }
-}
+import { CardTips } from './MarketStatusCards';
 
 export default class MarketStatusWEBS extends React.Component {
   constructor(props){
